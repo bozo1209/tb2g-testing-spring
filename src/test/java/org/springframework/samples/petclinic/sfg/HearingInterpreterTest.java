20fbbc5449a981bac7a097bde252efd65a73d14e
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-07-17 23:09:20 +0200

    junit 4 laurel test

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -2,17 +2,19 @@ package org.springframework.samples.petclinic.sfg;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
 
+    @Autowired
     HearingInterpreter hearingInterpreter;
-
-    @Before
-    public void setUp() throws Exception {
-        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
-    }
 
     @Test
     public void whatIHear() {
