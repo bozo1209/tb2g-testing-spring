803897ed11430b804c5d64a8cdf941bbbf8112f1
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-07-23 22:47:54 +0200

    spring mock mvc standalone setup

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,6 +25,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -38,12 +42,16 @@ class VetControllerTest {
 
     List<Vet> vetList = new ArrayList<>();
 
+    MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
 //        given
         vetList.add(new Vet());
 
         given(clinicService.findVets()).willReturn(vetList);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(vetController).build();
     }
 
     @Test
@@ -56,6 +64,14 @@ class VetControllerTest {
         then(clinicService).shouldHaveNoMoreInteractions();
         then(model).should().put(anyString(), any());
         then(model).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void testControllerShowVetList() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
     }
 
     @Test
